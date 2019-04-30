a1d218248534d8f0b690881c64f75d01a7c258f2
Kristýna Hájková <hajkova.kris13@gmail.com>
2019-04-30 13:09:19 +0200

    work in progress, version random decision

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const yesBox = document.getElementById("yes");
+const noBox = document.getElementById("no");
+
+function drinkCoffee(){
+    if (Math.random() >= 0.5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function renderResult(result){
+    if (result) {
+        yesBox.classList.remove("d-none");
+    } else {
+        noBox.classList.remove("d-none");
+    }
+};
+
+renderResult(drinkCoffee());
