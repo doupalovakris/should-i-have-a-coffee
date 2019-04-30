e8e5d67aac36c9bc44056605f07a6d1cea66eef0
Kristýna Hájková <hajkova.kris13@gmail.com>
2019-04-30 14:44:07 +0200

    work in progress, version decision according to hours

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -2,12 +2,14 @@ const yesBox = document.getElementById("yes");
 const noBox = document.getElementById("no");
 
 const drinkCoffee = () => {
-    if (Math.random() >= 0.5) {
+    let today = new Date();
+    let hours = today.getHours();
+    if ((hours >= 10 && hours < 12) || (hours >= 14 && hours < 16)) {
         return true;
     } else {
         return false;
     }
-}
+};
 
 const renderResult = (result) => {
     if (result) {
